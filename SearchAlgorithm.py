fc60f95bb55ed6720d1ca4758a92991e76595415
Adriana_Mc <adriana.mccabe@yahoo.com>
2021-03-11 18:02:19 -0500

    Can define specific sections in grid

--- SearchAlgorithm.py
+++ SearchAlgorithm.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 # Ask for the size of the search area
 meters = input("What is the squared footage of the area that we are searching? : ")
 # convert from str type to int type
@@ -12,24 +13,16 @@ for row in range(meters):
     for column in range(meters):
         grid[row].append('x')
 
-# Value that the while loop with go unto
-value = meters * meters
-
-# Counter for while loop
-counter = value;
-
-# While loop for the Archimedean Spiral
-#while counter > 0:
-for row in range(1):
-    grid.append([])
-    for column in range(1):
-        grid[row].append('0')
-
-          #  counter -= 5
-
 def print_grid(grid):
     for row in grid:
         print (" ".join(row))
 
 # Print out the grid
 print_grid(grid)
+
+# Value that the while loop with go unto
+value = meters * meters
+
+grid = np.array(grid)
+
+print (grid[1,1])
